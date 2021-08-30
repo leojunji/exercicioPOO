c7db128925fd1f716c68f8f1240ba73fab2b4217
Leonardo Junji <leojunji@outlook.com>
2021-08-30 20:22:35 -0300

    ex21_22 v1.1

--- src/exercicios/Exercicio21_22.java
+++ src/exercicios/Exercicio21_22.java
@@ -1,20 +1,17 @@
 package exercicios;
 
+
+/**Agora compare se o valor das duas variáveis acima é igual.*/
+
+
 public class Exercicio21_22 {
 
     public static void main(String[] args) {
 
-        /*Crie uma variável com valor de ponto flutuante com um valor
-         qualquer e  verifique se o valor desta variável está entre 1.99 e 5.99. */
 
-        float valorUm = 3f;
+        float valorUm = 1.99f;
+        float valorDois = 5.99f;
 
-        System.out.println("valorUm = " + valorUm);
-
-        if (valorUm >= 1.99f & 5.99f >= valorUm) {
-
-            System.out.println("1.99 <= valorUm <= 5.99");
-
-        }
+        System.out.printf("%s == %s --> %s", valorUm, valorDois, (valorUm==valorDois));
     }
 }
