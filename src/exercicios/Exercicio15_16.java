3e16a9ab8c72ccbc31e9ccba3516759ba80a3b4f
Leonardo Junji <leojunji@outlook.com>
2021-08-30 20:08:10 -0300

    ex15_16 v1.1

--- src/exercicios/Exercicio15_16.java
+++ src/exercicios/Exercicio15_16.java
@@ -2,13 +2,11 @@ package exercicios;
 
 import java.util.Scanner;
 
-/**
- Implemente um programa que receba dados
- digitados pelo usuário e atribua  a uma variável do tipo String
- e imprima a frase inserida no console.  */
+/**Concatene na variável criada acima a frase “ – Exemplo String”.   */
 
 
 public class Exercicio15_16 {
+
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
@@ -17,6 +15,9 @@ public class Exercicio15_16 {
         System.out.println("Digite o texto: ");
         textoUser = input.nextLine();
 
+        textoUser += "-Exemplo String";
+
         System.out.println("Texto digitado pelo usuário --> " + textoUser);
+
     }
 }
