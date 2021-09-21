219264a4b3f3cbd1c914443573781a00f6e1b916
Leonardo Junji <leojunji@outlook.com>
2021-09-21 00:16:04 -0300

    atualizando comentarios da clase metodos.java

--- src/exercicios_TDE_POO/Metodos.java
+++ src/exercicios_TDE_POO/Metodos.java
@@ -2,14 +2,17 @@ package exercicios_TDE_POO;
 
 public class Metodos {
 
+
+
+    /**Verifica se o valor é primo ou não
+     * @author Leonardo Junji Mukuno
+     * @param valor --> essa variável irá receber um número para ser verificado se é primo ou não
+     * @return se o número recebido for primo vai retornar true senão irá retornar false
+     * Se o valor for 1, 0, ou 2 o for não irá iniciar, sendo que se o a variável valor for 1 ou 0
+     * vai retornar false, mas se for 2 vai retornar true*/
     public boolean verificarPrimo(int valor){
 
-        /**
-         * Se o valor for 1, 0, ou 2 o for não irá iniciar, sendo que se o a variável valor for 1 ou 0
-         * vai retornar false, mas se for 2 vai retornar true
-         * @author Leonardo Junji Mukuno
-         * @param valor --> essa variável irá receber um número para ser verificado se é primo ou não
-         * @return se o número recebido for primo vai retornar true senão irá retornar false*/
+
         boolean valorPrimo = valor != 1 && valor != 0;
 
         for (int c = 2;c < valor;c++) {
